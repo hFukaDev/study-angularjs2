8b98c6efbb23bbbd6543359a063ac86dcff9a897
unknown <non@mail.com>
2016-10-30 21:38:16 +0900

    tutorial chapter1

---
+++ angular2-demo/app/environment_app.component.ts
@@ -0,0 +1,9 @@
+import {Component, View} from "angular2/core";
+@Component({
+    selector:'my-app'})
+@View({
+    template : '<h2>My First Anglar2 App'
+})
+export class AppComponent
+{
+}
---
+++ angular2-demo/app/environment_main.ts
@@ -0,0 +1,3 @@
+import {bootstrap} from "angular2/platform/browser"
+import {AppComponent} from "./environment_app.component"
+bootstrap(AppComponent);
